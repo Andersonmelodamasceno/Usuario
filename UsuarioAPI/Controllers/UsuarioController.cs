76d898acad29d32831d3680aab83cdce0e0a67a2
Andersonmelodamasceno <dinho.live01@gmail.com>
2023-10-23 08:34:15 -0300

    Atualização

--- UsuarioAPI/Controllers/UsuarioController.cs
+++ UsuarioAPI/Controllers/UsuarioController.cs
@@ -8,38 +8,55 @@ public class UsuarioController : ControllerBase
 {
     private List<User> _users = new List<User>(); // Simulação de uma lista de usuários em memória
 
+    private AppDbContext _appDbContext;
+    public UsuarioController( AppDbContext appDbContext)
+    {
+       _appDbContext= appDbContext;
+    }
     [HttpGet]
     public ActionResult<IEnumerable<User>> GetUsers()
     {
-        return Ok(_users);
+       var usuarios = _appDbContext.Users.ToList();  
+        return Ok(usuarios);
     }
 
     [HttpGet("{id}")]
-    public ActionResult<User> GetUser(Guid id)
+    public ActionResult<User> GetUser(string id)
     {
-        var user = _users.FirstOrDefault(u => u.Id == id);
+        var user = _appDbContext.Users.FirstOrDefault(u => u.Id == id);
         if (user == null)
             return NotFound();
 
         return Ok(user);
     }
 
+    //select * From users where name = 'Anderson'
+    [HttpGet("{name}")]
+    public ActionResult<List<User>> GetByUser(string name)
+    {
+        var users = _appDbContext.Users.Where(x => x.FirstName == name).ToList();
+        if (users == null)
+            return NotFound();
+
+        return Ok(users);
+    }
+
     [HttpPost]
     public ActionResult<User> CreateUser(User user)
     {
-        user.Id = Guid.NewGuid();
+        user.Id = string.Empty;
         _users.Add(user);
         return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
     }
 
     [HttpPut("{id}")]
-    public IActionResult UpdateUser(Guid id, User updatedUser)
+    public IActionResult UpdateUser(string id, User updatedUser)
     {
         var existingUser = _users.FirstOrDefault(u => u.Id == id);
         if (existingUser == null)
             return NotFound();
 
-        existingUser.Name = updatedUser.Name;
+        existingUser.FirstName = updatedUser.FirstName;
         existingUser.LastName = updatedUser.LastName;
         existingUser.Age = updatedUser.Age;
         existingUser.Sex = updatedUser.Sex;
@@ -51,7 +68,7 @@ public class UsuarioController : ControllerBase
     }
 
     [HttpDelete("{id}")]
-    public IActionResult DeleteUser(Guid id)
+    public IActionResult DeleteUser(string id)
     {
         var user = _users.FirstOrDefault(u => u.Id == id);
         if (user == null)
--- UsuarioAPI/Entities/UserProfile.cs
+++ UsuarioAPI/Entities/UserProfile.cs
@@ -2,8 +2,8 @@
 
 public class User
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string Id { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
     public string Sex { get; set; }
